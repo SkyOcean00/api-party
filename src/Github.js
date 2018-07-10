4069587200c6c4db580fe66709e7e67160c289a8
Cheng Qian <qianch@indiana.edu>
2018-07-10 12:07:29 -0500

    Add a controlled form to Github component

--- src/Github.js
+++ src/Github.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react'
 
+import './Github.css'
+
 class Github extends Component {
-      render() {
-        return (
-         <div className="Github">
-            GitHub (now a component)
+  state = {
+    username: '',
+  }
+
+  handleChange = ev => {
+    this.setState({ username: ev.target.value })
+  }
+
+  render() {
+    return (
+      <div className="Github">
+        <img
+          src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png"
+          alt="GitHub"
+          className="logo"
+        />
+
+        <form>
+          <div>
+            <input
+              required
+              autoFocus
+              type="text"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
           </div>
-        )
-      }
-    }
-    
-    export default Github
+          <div>
+            <button type="submit">
+              Look up GitHub user
+            </button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Github
